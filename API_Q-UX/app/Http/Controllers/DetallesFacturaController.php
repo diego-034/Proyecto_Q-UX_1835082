fa23724b603402e22e89abe7272e5f5fc6b48af1
diego-034 <diego-034>
2020-06-10 18:03:08 -0500

    Modelos, Controlladores, Migraciones

---
+++ API_Q-UX/app/Http/Controllers/DetallesFacturaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DetallesFactura;
+use Illuminate\Http\Request;
+
+class DetallesFacturaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DetallesFactura  $detallesFactura
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DetallesFactura $detallesFactura)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DetallesFactura  $detallesFactura
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DetallesFactura $detallesFactura)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DetallesFactura  $detallesFactura
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DetallesFactura $detallesFactura)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DetallesFactura  $detallesFactura
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DetallesFactura $detallesFactura)
+    {
+        //
+    }
+}
---
+++ API_Q-UX/database/factories/DetallesFacturaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\DetallesFactura;
+use Faker\Generator as Faker;
+
+$factory->define(DetallesFactura::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
