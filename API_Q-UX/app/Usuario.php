1df81220031e0954aeee8d2c992dd5499aec411b
diego-034 <diego-034>
2020-06-11 16:54:45 -0500

    Modelo Usuario

--- API_Q-UX/app/Usuario.php
+++ API_Q-UX/app/Usuario.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     protected $primaryKey = 'IdUsuario';
-    protected $fillable  = ['Nombre','FechaApertura'];
+    protected $fillable  = ['Nombres','Apellidos','Correo','Telefono','Celular','NIT','Contrasena','Estado'];
 }
