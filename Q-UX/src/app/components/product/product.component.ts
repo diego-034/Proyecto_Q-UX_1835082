1176998329b13a9e7fb8a902d978a0b2e9ddee6a
diego-034 <diego-034>
2020-06-11 19:04:04 -0500

    Servicio Productos

--- Q-UX/src/app/components/product/product.component.ts
+++ Q-UX/src/app/components/product/product.component.ts
@@ -5,11 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent {
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
+  peticion(){
+    let url = "http://127.0.0.1:8000/api/producto"
+    fetch(url, {
+      method: 'GET'
+  })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        (error) => {}
+      );
+  }
 }
