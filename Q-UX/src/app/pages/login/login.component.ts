7014188526175b1649e2ab3438c0f55eeee6350c
Jhon <ztheeban205@gmail.com>
2020-06-15 20:31:48 -0500

    algunos comentarios en login component

--- Q-UX/src/app/pages/login/login.component.ts
+++ Q-UX/src/app/pages/login/login.component.ts
@@ -10,16 +10,16 @@ import { LoginService } from 'src/app/services/login/login.service';
 
 export class LoginComponent implements OnInit {
 
-  vista = false;
+  vista = false; /* Se encarga de mostrarmos y ocultarnos el formulario, iagual el de boton */
   boton = true;
-  
+
   constructor(private LoginService: LoginService) { }
 
   ngOnInit(): void {
   }
 
   login(){
-    var email = document.querySelector("#email").nodeValue,
+    var email = document.querySelector("#email").nodeValue, /* Quite el jQuery porque me generaba errores, pero ya se puede volver a poner */
         password = document.querySelector("#password").nodeValue;
     var response = this.LoginService.login(email,password);
     console.log(response);
