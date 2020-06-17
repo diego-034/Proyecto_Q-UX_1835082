580a3d91328c64748a15166dfb2ddd0bf6790a97
diego-034 <diego-034>
2020-06-17 12:45:04 -0500

    jquery

--- Q-UX/src/app/pages/login/login.component.ts
+++ Q-UX/src/app/pages/login/login.component.ts
@@ -10,17 +10,14 @@ declare var $: any;
 
 export class LoginComponent implements OnInit {
 
-  vista = false; /* Se encarga de mostrarmos y ocultarnos el formulario, iagual el de boton */
-  boton = true;
-
   constructor(private LoginService: LoginService) { }
 
   ngOnInit(): void {
   }
 
   login(){
-    var email = document.querySelector("#email").nodeValue, /* Quite el jQuery porque me generaba errores, pero ya se puede volver a poner */
-        password = document.querySelector("#password").nodeValue;
+    var email = $("#email").val();
+    var password = $("#password").val();
     var response = this.LoginService.login(email,password);
     console.log(response);
   }
