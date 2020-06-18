0925c135e5674bbd70f8f50dc5bd56052fb29504
diego-034 <diego-034>
2020-06-18 15:19:46 -0500

    Children routes

--- Q-UX/src/app/app-routing.module.ts
+++ Q-UX/src/app/app-routing.module.ts
@@ -7,18 +7,30 @@ import { ClientsComponent } from './pages/clients/clients.component';
 import { LoginComponent } from './pages/login/login.component';
 //Servicios
 import {AuthService} from '../app/services/auth/auth.service';
+import { ClientComponent } from './components/client/client.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: 'products', component: ProductsComponent, canActivate:[AuthService]},
-  { path: 'clients', component: ClientsComponent,  canActivate:[AuthService]},
+  { path: 'products', component: ProductsComponent},
+  { path: 'admin',
+     children: [
+       {
+         path: 'home', // child route path
+         component: HomeComponent // child route component that the router renders
+       },
+       {
+         path: 'clients',
+         component: ClientComponent,  // another child route component that the router renders
+       }
+     ], canActivate:[AuthService] },
   { path: 'login', component: LoginComponent},
-  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+  { path: '**', pathMatch: 'full', redirectTo: 'home'},
+  { path: '',   redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
