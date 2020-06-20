bd35bb1e86508ffb92aaa513cd6ab3cc5bb5005a
Jhon <ztheeban205@gmail.com>
2020-06-19 20:39:12 -0500

    Componente my-clients

--- Q-UX/src/app/app.module.ts
+++ Q-UX/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductsComponent } from './pages/products/products.component';
 import { ClientsComponent } from './pages/admin/clients/clients.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MyHomeComponent } from './pages/admin/my-home/my-home.component';
+import { MyClientsComponent } from './pages/admin/my-clients/my-clients.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { MyHomeComponent } from './pages/admin/my-home/my-home.component';
     ProductsComponent,
     ClientsComponent,
     LoginComponent,
-    MyHomeComponent
+    MyHomeComponent,
+    MyClientsComponent
   ],
   imports: [
     BrowserModule,
